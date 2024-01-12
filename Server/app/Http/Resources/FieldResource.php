e5b130cc0829ee4f49c3c9cedcfd6f48ab515efb
Hajar-muhammad <01120898545ha@gmail.com>
2024-01-13 01:16:28 +0200

    edit feildResource

--- Server/app/Http/Resources/FieldResource.php
+++ Server/app/Http/Resources/FieldResource.php
@@ -16,6 +16,7 @@ class FieldResource extends JsonResource
     {
 
         return [
+            'id'=>$this->id,
             'name'=>$this->name,
             'price'=>$this->price,
             'projects'=>$this->projects,
